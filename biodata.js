77e755a168872bfb3ed89b338192181558ef4221
Fadhlih Girindra Putra <fadhlihgp@gmail.com>
2023-05-08 14:09:39 +0700

    initial commit

---
+++ biodata.js
@@ -0,0 +1,12 @@
+exports.getName = function(name) {
+    console.log("Nama: " + name);
+}
+exports.getBirthPlace = function (place) {
+    console.log("Tempat Lahir: " + place);
+}
+exports.getBirthDate = function(date) {
+    console.log("Tanggal Lahir: " + date);
+}
+exports.getAddress = function(address) {
+    console.log("Alamat: " + address);
+}
---
+++ moduleBiodata.js
@@ -0,0 +1,6 @@
+var biodata = require('./biodata');
+
+biodata.getName('Fadhlih Girindra Putra');
+biodata.getBirthPlace('Bekasi');
+biodata.getBirthDate('17 November 2000');
+biodata.getAddress('Jl Bonjol No. 30');
